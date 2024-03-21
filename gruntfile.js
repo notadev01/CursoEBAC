fa42874cd378bd1a82b12b9017a7383a3b1ef8d2
notadev01 <notadev01@example.com>
2024-03-21 18:45:44 -0300

    Initial commit.

---
+++ gruntfile.js
@@ -0,0 +1,23 @@
+module.exports = function(grunt) {
+    grunt.initConfig({
+        less: {
+            target: {
+                files: {
+                    'main.css': 'main.less'
+                }
+            }
+        },
+        uglify: {
+            target: {
+                files: {
+                    'main.min.js': 'main.js'
+                }
+            }
+        }
+    })
+
+    grunt.loadNpmTasks('grunt-contrib-less');
+    grunt.loadNpmTasks('grunt-contrib-uglify');
+
+    grunt.registerTask('default', ['less', uglify]);
+}
